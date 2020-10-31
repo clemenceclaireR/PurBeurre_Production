14176f37e07368451c4b3b0e7cd8a05d383f3466
clemenceclaireR <clemence.robinletellier@gmail.com>
2020-10-31 10:10:40 +0100

    Fixed bug on validation in register function

--- user/views.py
+++ user/views.py
@@ -24,11 +24,16 @@ def register(request):
 
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
-            validate_password(user_form.cleaned_data['password'])
-            new_user.set_password(user_form.cleaned_data['password'])
-            # generate a unique id in order to username not to be empty
-            new_user.username = uuid.uuid1()
-            new_user.save()
+            # enlever le try except avant le push test
+            try:
+                validate_password(user_form.cleaned_data['password'])
+                new_user.set_password(user_form.cleaned_data['password'])
+                # generate a unique id in order to username not to be empty
+                new_user.username = uuid.uuid1()
+                new_user.save()
+            except ValidationError as e:
+                user_form.add_error('password', e)
+                return render(request, 'registration/register.html', locals())
 
             return render(request,
                           'registration/register_done.html',
